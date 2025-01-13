bd32dfe84c6b371a3e85137a021a5bc1a1d28945
onhardcope <onhardcope@gmail.com>
2025-01-13 14:27:44 +0530

    Organised scenes and scripts into related folders

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_new_pressed() -> void:
-	get_tree().change_scene_to_file("res://create_save.tscn")
+	get_tree().change_scene_to_file("res://CreateSave/create_save.tscn")
 	
 func _on_quit_pressed() -> void:
 	get_tree().quit()
