af7cd0a182fdcd22cc9222cebc2802cca01200d6
onhardcope <onhardcope@gmail.com>
2025-01-13 09:26:09 +0530

    Added 'play tutorial' and 'play level-1' buttons to create_save scene

--- main_menu.gd
+++ main_menu.gd
@@ -1,11 +1,7 @@
 extends Node2D
 
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://kitchen.tscn")
+func _on_new_pressed() -> void:
+	get_tree().change_scene_to_file("res://create_save.tscn")
 	
-
-
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
