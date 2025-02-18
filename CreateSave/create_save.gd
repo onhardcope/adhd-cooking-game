38e29d69385f0098cbd12be035f6678a7a94efe1
Abhijith119 <169022585+Abhijith119@users.noreply.github.com>
2025-02-18 09:31:25 +0530

    added level select screen and back button

--- CreateSave/create_save.gd
+++ CreateSave/create_save.gd
@@ -11,5 +11,13 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_play_tutorial_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://tut.tscn") # Replace with function body.
+ # Replace with function body.
+
+
+func _on_level_select_1_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://LEVELS.tscn")
+	 # Replace with function body.
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://MainMenu/main_menu.tscn") # Replace with function body.
